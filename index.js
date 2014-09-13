f4c1034ba41774887ea62bcd60ed3d8bd86a6c0f
Nicholas E. Rabenau <nerab@gmx.at>
2014-09-13 20:53:21 +0200

    Add morgan request logger

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var morgan = require('morgan')
 var os = require("os");
 var PORT = 8080;
 
 var app = express();
+app.use(morgan('combined'))
 
 app.get('/', function (req, res) {
   res.send('Hello from ' + os.hostname() + ' at ' + new Date() + '\n');
